3fa5393d8926329aea534ac664645099cc2ae193
NahuelSaucedo <nahuel.saucedo@alumnos.frgp.utn.edu.ar>
2022-06-16 16:11:27 -0300

    Se Agrego la clase "Insumos"

---
+++ Modelo/Insumos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Insumos
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public int Tipo { get; set; }
+    }
+}
