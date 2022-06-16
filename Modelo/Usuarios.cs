6651bde18f1a52033e420471f412f576ee586320
NahuelSaucedo <nahuel.saucedo@alumnos.frgp.utn.edu.ar>
2022-06-16 16:12:05 -0300

    Se agrego la clase "usuarios"

---
+++ Modelo/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Usuarios
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string usuario { get; set; }
+        public string contrasenia { get; set; }
+        public int IDTipo { get; set; }
+    }
+}
