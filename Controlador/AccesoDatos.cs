9e1039805fb69ebe0a3c4d554f0b70fba10e5819
NahuelSaucedo <nahuel.saucedo@alumnos.frgp.utn.edu.ar>
2022-06-16 19:37:18 -0300

    AccesoDatos , Conexion con base de datos

---
+++ Controlador/AccesoDatos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Controlador
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection();
+            comando = new SqlCommand();
+        }
+
+
+        public void SetConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+            {
+                lector.Close();
+
+            }
+
+            conexion.Close();
+        }
+
+        public void EjecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
