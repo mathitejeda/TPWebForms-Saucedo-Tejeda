d5910c4dce3c1f08311e93dccbcd8b92acba19f0
mathitejeda <mathi.tejeda@gmail.com>
2022-06-23 12:13:57 -0300

    Clase nueva: Mesa negocio

---
+++ Controlador/MesaNegocio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class MesaNegocio
+    {
+        public List<MesaNegocio> listar()
+        {
+            List<MesaNegocio> listaMesas = new List<MesaNegocio>();
+
+            return listaMesas;
+        }
+    }
+}
