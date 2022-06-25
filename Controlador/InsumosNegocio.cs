57b78534374af8fe8fc7168123f971cab9613ca3
Nahuel Maximiliano Saucedo <nahuel.saucedo@alumnos.frgp.utn.edu.ar>
2022-06-25 17:55:56 -0300

    Lectura clase insumos

---
+++ Controlador/InsumosNegocio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Controlador
+{
+    class InsumosNegocio
+    {
+        public List<InsumosNegocio> listar()
+        {
+            List<InsumosNegocio> listaProductos = new List<InsumosNegocio>();
+            List<Insumos> lista = new List<Insumos>();
+            AccesoDatos datos = new AccesoDatos();
+            datos.SetConsulta("Select I.ID, I.nombre, I.PRECIO, I.STOCK, I.IDTIPO FROM INSUMOS AS I");
+            datos.EjecutarLectura();
+            while (datos.Lector.Read())
+            {
+                Insumos aux = new Insumos();
+                aux.ID = (int)datos.Lector["ID"];
+                aux.Nombre = (string)datos.Lector["Nombre"];
+                aux.Precio = (int)datos.Lector["Precio"];
+                aux.Stock = (int)datos.Lector["Stock"];
+                aux.Stock = (int)datos.Lector["IDTipo"];
+                lista.Add(aux);
+
+            }
+            return listaProductos;
+
+
+            
+        }
+    }
+}
