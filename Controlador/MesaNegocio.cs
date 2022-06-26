6391156ceb763276b7fa41cd2142f5819ad065d0
mathitejeda <mathi.tejeda@gmail.com>
2022-06-26 17:26:34 -0300

    Controlador -Solucionado error de datos en el controlador de las mesas

--- Controlador/MesaNegocio.cs
+++ Controlador/MesaNegocio.cs
@@ -12,7 +12,6 @@ namespace Controlador
         public List<Mesa> listar()
         {
             List<Mesa> listaMesas = new List<Mesa>();
-            List<Mesa> lista = new List<Mesa>();
             AccesoDatos datos = new AccesoDatos();
             datos.SetConsulta
                 (
@@ -23,10 +22,10 @@ namespace Controlador
             while (datos.Lector.Read())
             {
                 Mesa aux = new Mesa();
-                aux.ID = (long)datos.Lector["ID"];
+                aux.ID = (int)datos.Lector["ID"];
                 aux.Ocupada = (bool)datos.Lector["Ocupada"];
                 aux.IDMozo = (int)datos.Lector["IDMozo"];
-                lista.Add(aux);
+                listaMesas.Add(aux);
             }
             return listaMesas;
         }
